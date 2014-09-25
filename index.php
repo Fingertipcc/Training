b185456bb60227dbbc0f131f620e55882b892524
Administrator <Administrator@USER-20140924LQ>
2014-09-25 11:05:25 +0800

    fbxcb

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
  date_default_timezone_set("Asia/Shanghai");
 require_once 'config.php';
 require_once 'MySqlHelper.php';
-$he=new MySqlHelper();
-echo date(' Y-m-d H:i:s')."<br>".($he->connect($config)); 
 
+xcvxv
 ?>
 
