eac41cf9c1dd6cb5cfde0e4e0c6cda17ad615e6c
NENER <NENER@NENER-PC>
2014-09-25 10:08:44 +0800

    测试

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php 
 date_default_timezone_set("Asia/Shanghai");
-echo  date(' Y-m-d H:i:s');
 require_once 'config.php';
 require_once 'MySqlHelper.php';
 $he=new MySqlHelper();
-$sre=$he->connect($config);
-echo 'dsada'.$sre;
+echo date(' Y-m-d H:i:s')."<br>".($he->connect($config));
 ?>
 
