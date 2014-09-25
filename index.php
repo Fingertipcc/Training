1496a8e1101876b4be61fa5a8e4b58e64fe58d05
Administrator <Administrator@USER-20140924LQ>
2014-09-25 11:03:46 +0800

    fgsdg

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
  date_default_timezone_set("Asia/Shanghai");
-
+require_once 'config.php';
+require_once 'MySqlHelper.php';
+$he=new MySqlHelper();
+echo date(' Y-m-d H:i:s')."<br>".($he->connect($config)); 
 
 ?>
 
